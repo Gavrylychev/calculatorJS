78a7e5a618f6f83b5007b8624319b1bbe678519f
shamanbronya <gavrylychevalex@gmail.com>
2017-09-26 16:22:17 +0300

    again

--- calc.js
+++ calc.js
@@ -1,12 +1,8 @@
 window.onload = function() {
-  var current,
-      screen,
+  var screen,
       output,
       limit,
-      zero,
-      period,
-      isNewOperation = true,
-      operator;
+      isNewOperation = true;
 
   screen = document.getElementById("result");
   var elem = document.querySelectorAll(".num");
@@ -26,24 +22,6 @@ window.onload = function() {
      },false);
   }
 
-  // document.querySelector(".zero").addEventListener("click",function() {
-  //   zero = this.value;
-  //   if(screen.innerHTML === "") {
-  //     output = screen.innerHTML = zero;
-  //   }else if(screen.innerHTML === output) {
-  //     output = screen.innerHTML +=zero;
-  //   }
-  // },false);
-
-  // document.querySelector(".period").addEventListener("click",function() {
-  //   period = this.value;
-  //   if(screen.innerHTML === "") {
-  //     output = screen.innerHTML = screen.innerHTML.concat("0.");
-  //   }else if(screen.innerHTML === output) {
-  //     screen.innerHTML = screen.innerHTML.concat(".");
-  //   }
-  // },false);
-
   document.querySelector("#eqn-bg").addEventListener("click",function() {
     isNewOperation = true;
     if(screen.innerHTML === output) {
@@ -57,23 +35,9 @@ window.onload = function() {
    screen.innerHTML = "";
   },false);
 
-  // var elem1 = document.querySelectorAll(".operator");
-  //
-  // var len1 = elem1.length;
-  // for(var i = 0; i < len1; i++ ) {
-  //   elem1[i].addEventListener("click",function() {
-  //     operator = this.value;
-  //     if(screen.innerHTML === "") {
-  //       screen.innerHTML = screen.innerHTML.concat("");
-  //     }else if(output) {
-  //       screen.innerHTML = output.concat(operator);
-  //     }
-  //   },false);
-  // }
-
   function replaceAll(haystack, needle, replace) {
     return haystack.split(needle).join(replace);
-} // replace all fx
+}
 
 function reformat(s) {
     s = s.toLowerCase();
@@ -89,21 +53,21 @@ function reformat(s) {
     }
     while(s.charAt(0) == "+") s = s.substr(1);
     return s;
-} // standardize string format
+}
 
 function strContain(haystack, needle) {
     return haystack.indexOf(needle) > -1;
-} // custom true/false contains
+}
 
 function isParseable(n, minus) {
     return (!isNaN(n) || (n == "-" && !minus) || n == ".");
-} // determine if char should be added to side
+}
 
 function getSide(haystack, middle, direction, minus) {
     var i = middle + direction;
     var term = "";
-    var limit = (direction == -1) ? 0 : haystack.length; // set the stopping point, when you have gone too far
-    while (i * direction <= limit) { // while the current position is >= 0, or <= upper limit
+    var limit = (direction == -1) ? 0 : haystack.length;
+    while (i * direction <= limit) {
         if (isParseable(haystack[i], minus)) {
             if (direction == 1) term = term + haystack[i];
             else term = haystack[i] + term;
@@ -111,40 +75,39 @@ function getSide(haystack, middle, direction, minus) {
         } else { return term; }
     }
     return term;
-} // general fx to get two terms of any fx (multiply, add, etc)
+}
 
 function allocFx(eq, symbol, alloc, minus) {
-    minus = (typeof minus !== 'undefined'); // sometimes we want to capture minus signs, sometimes not
+    minus = (typeof minus !== 'undefined');
     if (strContain(eq, symbol)) {
         var middleIndex = eq.indexOf(symbol);
         var left = getSide(eq, middleIndex, -1, minus);
         var right = getSide(eq, middleIndex, 1, false);
-        eq = replaceAll(eq, left+symbol+right, alloc(left, right));
+        eq = replaceAll(eq, left + symbol + right, alloc(left, right));
     }
     return eq;
-} // fx to generically map a symbol to a function for parsing
+}
 
 function solveStr(eq) {
     firstNest:
-    while (strContain(eq, "(")) { // while the string has any parentheses
-        var first = eq.indexOf("("); // first get the earliest open parentheses
-        var last = first + 1; // start searching at the character after
-        var layer = 1; // we might run into more parentheses, so this integer will keep track
-        while (layer != 0) { // loop this until we've found the close parenthesis
-            if (eq[last] == ")") { // if we run into a close parenthesis, then subtract one from "layer"
+    while (strContain(eq, "(")) {
+        var first = eq.indexOf("(");
+        var last = first + 1;
+        var layer = 1;
+        while (layer != 0) {
+            if (eq[last] == ")") {
                 layer--;
-                if (layer == 0) break; // if it is the corresponding closing parenthesis for our outermost open parenthesis, then we can deal with this expression
+                if (layer == 0) break;
             }
-            else if (eq[last] == "(") { // if we see an open parenthesis, add one to "layer"
+            else if (eq[last] == "(") {
                 layer++;
             }
-            last++; // increment the character we're looking at
+            last++;
             if (last > eq.length) break firstNest;
         }
 
-        var nested = eq.substr(first + 1, last - first - 1); // get the expression between the parentheses
-
-        if (last + 1 <= eq.length) { // if there is exponentiation, change to a different symbol
+        var nested = eq.substr(first + 1, last - first - 1);
+        if (last + 1 <= eq.length) {
             if (eq[last + 1] == "^") {
                 eq = eq.substr(0, last + 1) + "&" + eq.substr((last+1)+1);
             }
@@ -152,10 +115,10 @@ function solveStr(eq) {
 
         var solvedStr = solveStr(nested);
         var preStr = "(" + nested + ")";
-        eq = eq.replace(preStr, solvedStr); // replace parenthetical with value
+        eq = eq.replace(preStr, solvedStr);
     }
-    while (strContain(eq, "^")) eq = allocFx(eq, "^", function(l, r) { return Math.pow(parseFloat(l),parseFloat(r)); }, false);
-    while (strContain(eq, "&")) eq = allocFx(eq, "&", function(l, r) { return Math.pow(parseFloat(l),parseFloat(r)); }); // account for things like (-3)^2
+    // while (strContain(eq, "^")) eq = allocFx(eq, "^", function(l, r) { return Math.pow(parseFloat(l),parseFloat(r)); }, false);
+    // while (strContain(eq, "&")) eq = allocFx(eq, "&", function(l, r) { return Math.pow(parseFloat(l),parseFloat(r)); });
     while (strContain(eq, "*") || strContain(eq, "/")) {
         var multiply = true;
         if (eq.indexOf("*") < eq.indexOf("/")) {
@@ -168,5 +131,5 @@ function solveStr(eq) {
     while (strContain(eq, "+")) eq = allocFx(eq, "+", function(l, r) { return parseFloat(l)+parseFloat(r); });
     screen.innerHTML = eq;
     return eq;
-}
+  }
 }
